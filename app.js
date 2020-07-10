2cc42468ebfdbd6f702f6fd00f1c4be58bdab9b6
hugleo <hugocanalli@gmail.com>
2020-07-10 13:52:18 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const zabbixUrl = '/zabbix';
+
+function generateUrl() {
+    graphId = $("#graphid").val();
+    var url = '';
+    if (graphId) {
+        url = zabbixUrl + '/zabbix.php';
+        var obj = { view_as: 'showgraph', action: 'charts.view', filter_search_type: '0', 'filter_graphids[]': graphId };
+        url = url + '?' + $.param(obj);
+    }
+    return url;
+}
+
+function loadIframe(iframeName, url) {
+    var $iframe = $('#' + iframeName);
+    if ( $iframe.length ) {
+        $iframe.attr('src',url);
+        return false;
+    }
+    return true;
+}
+
+$('#dropdowns').cascadingDropdown({
+    selectBoxes: [
+        {
+            selector: '.step1',
+            source: function(request, response) {
+                $.getJSON('app.php?step1', request, function(data) {
+                    var selectOnlyOption = data.length <= 1;
+                    response($.map(data, function(item, index) {
+                        return {
+                            label: item.label,
+                            value: item.value,
+                            selected: index == 0
+                        };
+                    }));
+                }).fail(function() { // Maybe login cookie problem
+                    url = zabbixUrl + '/index.php';
+                    $(location).attr('href', url); // Redirect to zabbix login page
+                });
+            }
+        },
+
+        {
+            selector: '.step2',
+            requires: ['.step1'],
+            source: function(request, response) {
+                $.getJSON('app.php', request, function(data) {
+                    var selectOnlyOption = data.length <= 1;
+                    response($.map(data, function(item, index) {
+                        return {
+                            label: item.label,
+                            value: item.value,
+                            selected: selectOnlyOption
+                        };
+                    }));
+                });
+            }
+        },
+        {
+            selector: '.step3',
+            requires: ['.step1', '.step2'],
+            requireAll: true,
+            source: function(request, response) {
+                $.getJSON('app.php', request, function(data) {
+                    response($.map(data, function(item, index) {
+                        return {
+                            label: item.label,
+                            value: item.value,
+                            selected: index == 0
+                        };
+                    }));
+                });
+            },
+            onChange: function(event, value, requiredValues, requirementsMet){
+                url = generateUrl();
+        loadIframe('zabbixifr', url);
+            }
+        }
+    ]
+});
+
+
---
+++ app.php
@@ -0,0 +1,79 @@
+<?php
+    require_once __DIR__.'/lib/vendor/autoload.php';
+    use ZabbixApi\ZabbixApi;
+    use ZabbixApi\Exception;
+
+    $zabbixUrl = 'http://yourdomain/zabbix';
+
+    $hostgroup = '';
+    $hostname = '';
+    $graphname = '';
+
+    foreach ($_GET as $key => $value) {
+        if ($key == 'hostgroup') {
+			if (!empty($value)) {
+				$hostgroup = $value;
+            }
+        }
+        elseif ($key == 'hostname') {
+            if (!empty($value)) {
+                $hostname = $value;
+			}
+		}
+    }
+
+    $api = new ZabbixApi($zabbixUrl . '/api_jsonrpc.php', '', '', '', '', $_COOKIE['zbx_sessionid']);
+    $api->setDefaultParams(array(
+        'output' => 'extend',
+        'real_hosts' => 'true'
+    ));
+
+    if (empty($hostgroup)) {
+        $hostgroups = $api->hostgroupGet(array(
+            'sortfield' => 'name'
+        ));
+
+        $arr = array();
+        foreach ($hostgroups as $hg) {
+            $item = new stdClass;
+            $item->label = $hg->name;
+            $item->value = $hg->groupid;
+        $arr[] = $item;
+        }
+
+        echo json_encode($arr);
+    }
+    elseif (empty($hostname)) {
+        $hosts = $api->hostGet(array(
+        'sortfield' => 'host',
+        'groupids' => array($hostgroup)
+        ));
+
+        $arr = array();
+        foreach ($hosts as $h) {
+            $item = new stdClass;
+            $item->label = $h->host;
+            $item->value = $h->hostid;
+            $arr[] = $item;
+        }
+
+        echo json_encode($arr);
+    }
+    elseif (empty($graphname)) {
+        $graphs = $api->graphGet(array(
+            'sortfield' => 'name',
+            'hostids' => array($hostname)
+        ));
+
+        $arr = array();
+        foreach ($graphs as $g) {
+            $item = new stdClass;
+            $item->label = $g->name;
+            $item->value = $g->graphid;
+            $arr[] = $item;
+        }
+
+        echo json_encode($arr);
+    }
+
+?>
